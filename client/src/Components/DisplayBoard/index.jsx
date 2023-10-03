3a7b8e0ea7e897c8b538455583b31f34db204a68
Yaqi Xie <91613914+yaqiX@users.noreply.github.com>
2023-10-03 15:14:04 -0700

    add display board

---
+++ client/src/Components/DisplayBoard/index.jsx
@@ -0,0 +1,17 @@
+import "./index.scss";
+
+const DisplayBoard = () => {
+    return (
+        <div className="boxBoard">
+            <div className="box box1">Box 1</div>
+            <div className="box box2">Box 2</div>
+            <div className="box box3">Box 3</div>
+            <div className="box box4">Box 4</div>
+            <div className="box box5">Box 5</div>
+            <div className="box box6">Box 6</div>
+            <div className="box box7">Box 7</div>
+        </div>
+    )
+}
+
+export default DisplayBoard;
--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,11 +1,13 @@
 import Slider from "../../Components/Slider";
 import FeaturedProduct from "../../Components/FeaturedProduct";
+import DisplayBoard from "../../Components/DisplayBoard";
 
 const Home = () => {
     return(
         <div className="home">
             <Slider/>
             <FeaturedProduct/>
+            <DisplayBoard/>
         </div>
     )
 }
