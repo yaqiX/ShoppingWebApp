a7247ccbb15ce857744068a4198bc21599327908
Yaqi Xie <91613914+yaqiX@users.noreply.github.com>
2023-10-04 09:51:28 -0700

    style board

--- client/src/Components/DisplayBoard/index.jsx
+++ client/src/Components/DisplayBoard/index.jsx
@@ -1,17 +1,71 @@
 import "./index.scss";
+import { Link } from "react-router-dom";
 
 const DisplayBoard = () => {
     return (
         <div className="boxBoard">
-            <div className="box box1">Box 1</div>
-            <div className="box box2">Box 2</div>
-            <div className="box box3">Box 3</div>
-            <div className="box box4">Box 4</div>
-            <div className="box box5">Box 5</div>
-            <div className="box box6">Box 6</div>
-            <div className="box box7">Box 7</div>
+            <div className="box box1">
+                <img src="https://images.unsplash.com/photo-1554412933-514a83d2f3c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDN8fGZhc2hpb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60
+"/>
+                <button>
+                    {/* <Link to="/catogories/sale">Sale</Link> */}
+                    sale
+                </button>
+            </div>
+            <div className="box box2">
+                <img src="https://images.unsplash.com/photo-1537832816519-689ad163238b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZmFzaGlvbnxlbnwwfDB8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60" alt="button" />
+                <button>
+                    {/* <Link to="/catogories/sale">Sale</Link> */}
+                    sale
+                </button>
+            </div>
+            <div className="box box3">
+                <img src="https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFzaGlvbnxlbnwwfDB8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60" alt="button" />
+                <button>
+                    {/* <Link to="/catogories/sale">Sale</Link> */}
+                    sale
+                </button>
+            </div>
+            <div className="box box4">
+                <img src="https://images.unsplash.com/photo-1558769132-cb1aea458c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZmFzaGlvbnxlbnwwfDB8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60" alt="button" />
+                <button>
+                    {/* <Link to="/catogories/sale">Sale</Link> */}
+                    sale
+                </button>
+            </div>
+            <div className="box box5">
+                <img src="https://images.unsplash.com/photo-1556905055-8f358a7a47b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y2xvdGhlc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60" alt="button" />
+                <button>
+                    {/* <Link to="/catogories/sale">Sale</Link> */}
+                    sale
+                </button>
+            </div>
+            <div className="box box6">
+                <img src="https://images.unsplash.com/photo-1532453288672-3a27e9be9efd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60" alt="button" />
+                <button>
+                    {/* <Link to="/catogories/sale">Sale</Link> */}
+                    sale
+                </button>
+            </div>
+            <div className="box box7">
+                <img src="https://images.unsplash.com/photo-1591047139829-d91aecb6caea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60" alt="button" />
+                <button>
+                    {/* <Link to="/catogories/sale">Sale</Link> */}
+                    sale
+                </button>
+            </div>
         </div>
     )
 }
 
 export default DisplayBoard;
+
+
+// https://images.unsplash.com/photo-1554412933-514a83d2f3c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDN8fGZhc2hpb258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60
+// https://images.unsplash.com/photo-1483985988355-763728e1935b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZmFzaGlvbnxlbnwwfDB8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60
+// https://images.unsplash.com/photo-1537832816519-689ad163238b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8ZmFzaGlvbnxlbnwwfDB8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60
+// https://images.unsplash.com/photo-1558769132-cb1aea458c5e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8ZmFzaGlvbnxlbnwwfDB8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60
+// https://images.unsplash.com/photo-1556905055-8f358a7a47b2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y2xvdGhlc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60
+// https://images.unsplash.com/photo-1542060748-10c28b62716f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y2xvdGhlc3xlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=900&q=60
+// https://images.unsplash.com/photo-1532453288672-3a27e9be9efd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60
+// https://images.unsplash.com/photo-1591047139829-d91aecb6caea?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGNsb3RoZXN8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=900&q=60
