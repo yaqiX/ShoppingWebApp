471442a46fa887b48c39f8dad1372d949fa9ff30
Yaqi Xie <91613914+yaqiX@users.noreply.github.com>
2023-10-07 11:26:11 -0700

    update api

--- client/src/Components/FeaturedProduct/index.jsx
+++ client/src/Components/FeaturedProduct/index.jsx
@@ -1,43 +1,69 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Card from "../Card";
-import "./index.scss"
+import "./index.scss";
 
 const FeaturedProduct = () => {
+  const [products, setProducts] = useState([]);
 
-    const [products, setProducts] = useState([])
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:1337/api/categories`);
+        console.log("fetch success", response.data);
+        
+        // Update the state with the fetched data
+        setProducts(response.data); // Assuming your response data is an array
+        console.log(typeof response) 
+      } catch (err) {
+        console.log("Error fetching data:", err);
+      }
+    };
+  
+    fetchData();
+  }, []);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       // const response = await axios.get(`http://localhost:1337/api/categories`).then(
+          
+  //       // );
+  //       const response = await axios.get(`http://localhost:1337/api/categories`).then(
+  //         response => {
+  //           console.log("fetch success",response.data);
+  //         }
+  //       );
+      
+  //       // Update the state with the fetched data
+       
+  //     } catch (err) {
+  //       console.log("Error fetching data:", err);
+  //     }
+  //   };
 
-    const apiUrl = process.env.REACT_APP_API_URL;
-    const apiKey = process.env.REACT_APP_API_TOKEN;
-    
-    useEffect(() => {
-        const fetchData = async () => {
-            try{
-                const data = await axios.get(apiUrl + "/categories",{
-                    Authorization: "bearer " + apiKey,
-                });
-                console.log("data",data);
-            } catch(err){
-                console.log("errordata",err);
-            }
-        };
-        fetchData();
-    },[])
+  //   fetchData();
+  // },[]); 
 
-
-    return (
-        <div className="featuredProduct">
-            <div className="top">
-                <h1>Products</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum voluptates quasi quo aperiam nemo minus natus quidem rerum saepe omnis maiores, commodi beatae eligendi neque ipsam iusto suscipit ipsa reiciendis?</p>
-            </div>
-            <div className="pics">
-                {picData.map((product) => (
-                    <Card key={product.id} item={product}/>
-                ))}
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="featuredProduct">
+      <div className="top">
+        <h1>Products</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+          voluptates quasi quo aperiam nemo minus natus quidem rerum saepe omnis
+          maiores, commodi beatae eligendi neque ipsam iusto suscipit ipsa
+          reiciendis?
+        </p>
+      </div>
+      <div className="pics">
+        {/* {products.map((product) => (
+          <Card key={product.id} item={product} />
+        ))} */}
+        {products.data}
+        
+      </div>
+    </div>
+  );
+};
 
 export default FeaturedProduct;
